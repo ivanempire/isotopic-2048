ceb5a208b30d7d06bc026e33fd72f6d4296b315d
Ivan Melnikov <ivanempire@gmail.com>
2025-07-27 22:16:22 -0700

    Text component, some styling TODOs

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -12,6 +12,19 @@
     line-height: 47px;
   "
 >
+
+<!--    .score-container:after, .best-container:after {-->
+<!--    position: absolute; TODO: This needs to be the label -->
+<!--    width: 100%;-->
+<!--    top: 10px;-->
+<!--    left: 0;-->
+<!--    text-transform: uppercase;-->
+<!--    font-size: 13px;-->
+<!--    line-height: 13px;-->
+<!--    text-align: center;-->
+<!--    color: #eee4da;-->
+<!--}-->
     <div style="line-height: 1;">{label}</div>
     <div style="line-height: 1;">{value}</div>
 </div>
+
