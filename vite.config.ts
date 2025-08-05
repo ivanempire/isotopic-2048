43cad06a426c4cfcb19fded063247ad7e0170a92
Ivan Melnikov <ivanempire@gmail.com>
2025-08-04 20:46:11 -0700

    Testing PWA changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,31 +4,41 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { SvelteKitPWA } from "@vite-pwa/sveltekit";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), SvelteKitPWA()]
+	plugins: [tailwindcss(), sveltekit(), SvelteKitPWA({
+		strategies: "generateSW",
+		selfDestroying: true,
+		manifest: {
+			short_name: "Isotopic-2048",
+			name: "A radioactive version of 2048",
+			start_url: "/",
+			scope: "/",
+			display: "standalone",
+			theme_color: "#FAF8EF",
+			background_color: "#FAF8EF",
+			icons: [
+				{
+					src: "/pwa-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "/pwa-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+				{
+					src: "/pwa-maskable-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+					purpose: "maskable",
+				},
+				{
+					src: "/pwa-maskable-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+					purpose: "maskable",
+				},
+			],
+		},
+	})]
 });
-
-//export default defineConfig({
-//   plugins: [
-//     VitePWA({
-//       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-//       manifest: {
-//         name: 'My Awesome App',
-//         short_name: 'MyApp',
-//         description: 'My Awesome App description',
-//         theme_color: '#ffffff',
-//         icons: [
-//           {
-//             src: 'pwa-192x192.png',
-//             sizes: '192x192',
-//             type: 'image/png'
-//           },
-//           {
-//             src: 'pwa-512x512.png',
-//             sizes: '512x512',
-//             type: 'image/png'
-//           }
-//         ]
-//       }
-//     })
-//   ]
-// })
